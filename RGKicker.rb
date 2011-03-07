cba7c215dc93521224dab6029f9c15b93dafd850
Richard Shade <rshade@rightscale.com>
2011-03-07 16:43:42 -0600

    updated kicker

--- RGKicker.rb
+++ RGKicker.rb
@@ -1,13 +1,20 @@
 class RGKicker
   def do_work(message_env, message)
     starttime = Time.now
-    s3_downloaded_list = message_env[:s3_downloaded_list]
-    conversion_type = message_env[:conversion_type]
+    s3_downloaded_ist = message_env[:s3_downloaded_list]
+    
     f = File.new('/mnt/rightgrid/img.log', 'a+')
-    f.write "/usr/bin/ruby -d image_convert.rb -t #{conversion_type} -i #{s3_downloaded_list[s3_downloaded_list.keys.first].gsub(/\(/, '\(').gsub(/\)/, '\)')} -o #{message_env[:output_dir]}/#{File.basename(s3_downloaded_list[s3_downloaded_list.keys.first].gsub(/\(/, '\(').gsub(/\)/, '\)'))} \n"
+    f.write "/usr/bin/ruby -d geocode.rb -a #{message_env[:address}} -t lat\n"
     f.close
-		puts `/usr/bin/ruby -d image_convert.rb -t #{conversion_type} -i #{s3_downloaded_list[s3_downloaded_list.keys.first].gsub(/\(/, '\(').gsub(/\)/, '\)')} -o #{message_env[:output_dir]}/#{File.basename(s3_downloaded_list[s3_downloaded_list.keys.first].gsub(/\(/, '\(').gsub(/\)/, '\)'))}`
-
+		lat=`/usr/bin/ruby -d geocode.rb -a #{message_env[:address}} -t lat`
+		f = File.new('/mnt/rightgrid/img.log', 'a+')
+    f.write "/usr/bin/ruby -d geocode.rb -a #{message_env[:address}} -t lng\n"
+    f.close
+		lng=`/usr/bin/ruby -d geocode.rb -a #{message_env[:address}} -t lng`
+		f = File.new('/mnt/rightgrid/img.log', 'a+')
+    f.write "/usr/bin/ruby -d census.rb -lat #{lat} -lng #{lng}"
+    f.close
+    census_tract_id=`/usr/bin/ruby -d census.rb -lat #{lat} -lng #{lng}`
     finishtime = Time.now
     rg_result=0
     if $?.exitstatus == 0
@@ -27,8 +34,10 @@ class RGKicker
       :starttime => starttime,
       :finishtime => finishtime,
       :created_at => message_env[:created_at],
-      :output => message_env[:ec2_instance_id]
+      :address => res[1],
+	  	:lat => "#{lat}",
+	  	:lng => "#{lng}",
+	  	:census_tract_id => "#{census_tract_id}"
      }
   end
-
 end
